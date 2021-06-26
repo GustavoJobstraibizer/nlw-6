916acf6ed4f196c1146562d044b2fc0bd8a2eba2
Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>
2021-06-26 09:45:49 -0300

    test deploy 4
    Signed-off-by: Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { AdminRoom } from "./pages/AdminRoom";
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { Test } from "./pages/Test";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <AuthContextProvider>
         <ThemeContextProvider>
           <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path="/" exact component={Test} />
             <Route path="/rooms/new" exact component={NewRoom} />
             <Route path="/rooms/:id" component={Room} />
             <Route path="/admin/rooms/:id" component={AdminRoom} />
---
+++ src/pages/Test.tsx
@@ -0,0 +1,3 @@
+export function Test() {
+  return <h1>Hello world</h1>;
+}
