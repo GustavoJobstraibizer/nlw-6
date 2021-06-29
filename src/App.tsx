7e9ff578cb63b631fe25c9fa648c9c9f6cd3a17b
Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>
2021-06-28 22:58:34 -0300

    deploy without firebase config 3
    Signed-off-by: Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
-import { AdminRoom } from "./pages/AdminRoom";
-// import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from "./pages/Room";
 import { Test } from "./pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
-      <AuthContextProvider>
-        <ThemeContextProvider>
-          <Switch>
-            <Route path="/" exact component={Test} />
-            <Route path="/rooms/new" exact component={NewRoom} />
-            <Route path="/rooms/:id" component={Room} />
-            <Route path="/admin/rooms/:id" component={AdminRoom} />
-          </Switch>
-        </ThemeContextProvider>
-      </AuthContextProvider>
+      {/* <AuthContextProvider> */}
+      <ThemeContextProvider>
+        <Switch>
+          <Route path="/" exact component={Test} />
+          {/* <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} /> */}
+        </Switch>
+      </ThemeContextProvider>
+      {/* </AuthContextProvider> */}
     </BrowserRouter>
   );
 }
