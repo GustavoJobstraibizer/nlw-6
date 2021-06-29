706d3e005f47b6b5b1d389d623cc79240d64c827
Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>
2021-06-28 23:00:09 -0300

    deploy without firebase config / add authcontext
    Signed-off-by: Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { Test } from "./pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthContextProvider> */}
-      <ThemeContextProvider>
-        <Switch>
-          <Route path="/" exact component={Test} />
-          {/* <Route path="/rooms/new" exact component={NewRoom} />
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Switch>
+            <Route path="/" exact component={Test} />
+            {/* <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
           <Route path="/admin/rooms/:id" component={AdminRoom} /> */}
-        </Switch>
-      </ThemeContextProvider>
-      {/* </AuthContextProvider> */}
+          </Switch>
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
