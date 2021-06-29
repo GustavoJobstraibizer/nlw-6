b4559e2c837af8138698baf063c0afd8065f1365
Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>
2021-06-28 23:14:42 -0300

    remmoving themecontext
    Signed-off-by: Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
-import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { Test } from "./pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Switch>
-            <Route path="/" exact component={Test} />
-            {/* <Route path="/rooms/new" exact component={NewRoom} />
+      {/* <ThemeContextProvider> */}
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Test} />
+          {/* <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
           <Route path="/admin/rooms/:id" component={AdminRoom} /> */}
-          </Switch>
-        </AuthContextProvider>
-      </ThemeContextProvider>
+        </Switch>
+      </AuthContextProvider>
+      {/* </ThemeContextProvider> */}
     </BrowserRouter>
   );
 }
